9c245a9f889fb93980fd8aba7e871dae8c1ae5ab
Aaron Brady <aaron.brady@shopify.com>
2023-09-17 18:20:18 -0400

    clang-formatted files

--- ft8.c
+++ ft8.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <pigpio.h>
 #include <inttypes.h>
-#include <unistd.h>
-#include <sys/time.h>
+#include <pigpio.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
 #include "si5351a.h"
 
 #define I2C_WRITE 0x60
-#define I2C_READ  0x61
+#define I2C_READ 0x61
 #define I2C_BUS 1
 
-#include "ft8_lib/ft8/pack.h"
-#include "ft8_lib/ft8/encode.h"
 #include "ft8_lib/ft8/constants.h"
+#include "ft8_lib/ft8/encode.h"
+#include "ft8_lib/ft8/pack.h"
 
 //
 // Set up specified PLL with mult, num and denom
@@ -21,11 +22,11 @@
 // num is 0..1,048,575 (0xFFFFF)
 // denom is 0..1,048,575 (0xFFFFF)
 //
-void setupPLL(int i2c, uint8_t pll, uint8_t mult, uint32_t num, uint32_t denom)
-{
-    uint32_t P1;					// PLL config register P1
-    uint32_t P2;					// PLL config register P2
-    uint32_t P3;					// PLL config register P3
+void setupPLL(int i2c, uint8_t pll, uint8_t mult, uint32_t num,
+              uint32_t denom) {
+    uint32_t P1;  // PLL config register P1
+    uint32_t P2;  // PLL config register P2
+    uint32_t P3;  // PLL config register P3
 
     P1 = (uint32_t)(128 * ((float)num / (float)denom));
     P1 = (uint32_t)(128 * (uint32_t)(mult) + P1 - 512);
@@ -38,7 +39,8 @@ void setupPLL(int i2c, uint8_t pll, uint8_t mult, uint32_t num, uint32_t denom)
     i2cWriteByteData(i2c, pll + 2, (P1 & 0x00030000) >> 16);
     i2cWriteByteData(i2c, pll + 3, (P1 & 0x0000FF00) >> 8);
     i2cWriteByteData(i2c, pll + 4, (P1 & 0x000000FF));
-    i2cWriteByteData(i2c, pll + 5, ((P3 & 0x000F0000) >> 12) | ((P2 & 0x000F0000) >> 16));
+    i2cWriteByteData(i2c, pll + 5,
+                     ((P3 & 0x000F0000) >> 12) | ((P2 & 0x000F0000) >> 16));
     i2cWriteByteData(i2c, pll + 6, (P2 & 0x0000FF00) >> 8);
     i2cWriteByteData(i2c, pll + 7, (P2 & 0x000000FF));
 }
@@ -48,24 +50,24 @@ void setupPLL(int i2c, uint8_t pll, uint8_t mult, uint32_t num, uint32_t denom)
 // R divider is the bit value which is OR'ed onto the appropriate register, it
 // is a #define in si5351a.h
 //
-void setupMultisynth(int i2c, uint8_t synth, uint32_t divider, uint8_t rDiv)
-{
-    uint32_t P1;					// Synth config register P1
-    uint32_t P2;					// Synth config register P2
-    uint32_t P3;					// Synth config register P3
+void setupMultisynth(int i2c, uint8_t synth, uint32_t divider, uint8_t rDiv) {
+    uint32_t P1;  // Synth config register P1
+    uint32_t P2;  // Synth config register P2
+    uint32_t P3;  // Synth config register P3
 
     P1 = 128 * divider - 512;
-    P2 = 0;							// P2 = 0, P3 = 1 forces an integer value for the divider
+    P2 = 0;  // P2 = 0, P3 = 1 forces an integer value for the divider
     P3 = 1;
 
-    i2cWriteByteData(i2c, synth + 0,   (P3 & 0x0000FF00) >> 8);
-    i2cWriteByteData(i2c, synth + 1,   (P3 & 0x000000FF));
-    i2cWriteByteData(i2c, synth + 2,   ((P1 & 0x00030000) >> 16) | rDiv);
-    i2cWriteByteData(i2c, synth + 3,   (P1 & 0x0000FF00) >> 8);
-    i2cWriteByteData(i2c, synth + 4,   (P1 & 0x000000FF));
-    i2cWriteByteData(i2c, synth + 5,   ((P3 & 0x000F0000) >> 12) | ((P2 & 0x000F0000) >> 16));
-    i2cWriteByteData(i2c, synth + 6,   (P2 & 0x0000FF00) >> 8);
-    i2cWriteByteData(i2c, synth + 7,   (P2 & 0x000000FF));
+    i2cWriteByteData(i2c, synth + 0, (P3 & 0x0000FF00) >> 8);
+    i2cWriteByteData(i2c, synth + 1, (P3 & 0x000000FF));
+    i2cWriteByteData(i2c, synth + 2, ((P1 & 0x00030000) >> 16) | rDiv);
+    i2cWriteByteData(i2c, synth + 3, (P1 & 0x0000FF00) >> 8);
+    i2cWriteByteData(i2c, synth + 4, (P1 & 0x000000FF));
+    i2cWriteByteData(i2c, synth + 5,
+                     ((P3 & 0x000F0000) >> 12) | ((P2 & 0x000F0000) >> 16));
+    i2cWriteByteData(i2c, synth + 6, (P2 & 0x0000FF00) >> 8);
+    i2cWriteByteData(i2c, synth + 7, (P2 & 0x000000FF));
 }
 
 //
@@ -73,8 +75,7 @@ void setupMultisynth(int i2c, uint8_t synth, uint32_t divider, uint8_t rDiv)
 // Example: si5351aOutputOff(SI_CLK0_CONTROL);
 // will switch off output CLK0
 //
-void si5351aOutputOff(uint8_t clk)
-{
+void si5351aOutputOff(uint8_t clk) {
     int i2c = i2cOpen(I2C_BUS, I2C_WRITE, 0);
 
     i2cWriteByteData(i2c, clk, 0x80);
@@ -82,7 +83,7 @@ void si5351aOutputOff(uint8_t clk)
     i2cClose(i2c);
 }
 
-// 
+//
 // Set CLK0 output ON and to the specified frequency
 // Frequency is in the range 1MHz to 150MHz
 // Example: si5351aSetFrequency(10000000);
@@ -90,8 +91,7 @@ void si5351aOutputOff(uint8_t clk)
 //
 // This example sets up PLL A and MultiSynth 0 and produces the output on CLK0
 //
-void si5351aSetFrequency(float frequency)
-{
+void si5351aSetFrequency(float frequency) {
     uint32_t pllFreq;
     uint32_t xtalFreq = XTAL_FREQ;
     uint32_t l;
@@ -103,31 +103,36 @@ void si5351aSetFrequency(float frequency)
 
     int i2c = i2cOpen(I2C_BUS, I2C_WRITE, 0);
 
-    divider = 900000000 / frequency;// Calculate the division ratio. 900,000,000 is the maximum internal 
+    divider = 900000000 / frequency;  // Calculate the division ratio.
+                                      // 900,000,000 is the maximum internal
     // PLL frequency: 900MHz
-    if (divider % 2) divider--;		// Ensure an even integer division ratio
+    if (divider % 2) divider--;  // Ensure an even integer division ratio
 
-    pllFreq = divider * frequency;	// Calculate the pllFrequency: the divider * desired output frequency
+    pllFreq = divider * frequency;  // Calculate the pllFrequency: the divider *
+                                    // desired output frequency
 
-    mult = pllFreq / xtalFreq;		// Determine the multiplier to get to the required pllFrequency
-    l = pllFreq % xtalFreq;			// It has three parts:
-    f = l;							// mult is an integer that must be in the range 15..90
-    f *= 1048575;					// num and denom are the fractional parts, the numerator and denominator
-    f /= xtalFreq;					// each is 20 bits (range 0..1048575)
-    num = f;						// the actual multiplier is  mult + num / denom
-    denom = 1048575;				// For simplicity we set the denominator to the maximum 1048575
+    mult = pllFreq / xtalFreq;  // Determine the multiplier to get to the
+                                // required pllFrequency
+    l = pllFreq % xtalFreq;     // It has three parts:
+    f = l;          // mult is an integer that must be in the range 15..90
+    f *= 1048575;   // num and denom are the fractional parts, the numerator and
+                    // denominator
+    f /= xtalFreq;  // each is 20 bits (range 0..1048575)
+    num = f;        // the actual multiplier is  mult + num / denom
+    denom = 1048575;  // For simplicity we set the denominator to the maximum
+                      // 1048575
 
     // Set up PLL A with the calculated multiplication ratio
     setupPLL(i2c, SI_SYNTH_PLL_A, mult, num, denom);
-    // Set up MultiSynth divider 0, with the calculated divider. 
-    // The final R division stage can divide by a power of two, from 1..128. 
-    // reprented by constants SI_R_DIV1 to SI_R_DIV128 (see si5351a.h header file)
-    // If you want to output frequencies below 1MHz, you have to use the 
+    // Set up MultiSynth divider 0, with the calculated divider.
+    // The final R division stage can divide by a power of two, from 1..128.
+    // reprented by constants SI_R_DIV1 to SI_R_DIV128 (see si5351a.h header
+    // file) If you want to output frequencies below 1MHz, you have to use the
     // final R division stage
     setupMultisynth(i2c, SI_SYNTH_MS_0, divider, SI_R_DIV_1);
-    // Reset the PLL. This causes a glitch in the output. For small changes to 
+    // Reset the PLL. This causes a glitch in the output. For small changes to
     // the parameters, you don't need to reset the PLL, and there is no glitch
-    i2cWriteByteData(i2c, SI_PLL_RESET, 0xA0);	
+    i2cWriteByteData(i2c, SI_PLL_RESET, 0xA0);
     // Finally switch on the CLK0 output (0x4F)
     // and set the MultiSynth0 input to be PLL A
     i2cWriteByteData(i2c, SI_CLK0_CONTROL, 0x4F | SI_CLK_SRC_PLL_A);
@@ -159,16 +164,17 @@ void generate_ft8_tones(const char *msg, uint8_t *buffer) {
 int main(int argc, char **argv) {
     gpioInitialise();
     // CQ VE3NNE FN25
-    uint8_t symbols[] = { 3, 1, 4, 0, 6, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 4, 0,
-        2, 6, 0, 0, 0, 3, 3, 0, 6, 0, 3, 6, 3, 3, 5, 6, 4, 3, 1, 4, 0, 6, 5, 2,
-        1, 7, 4, 6, 4, 2, 0, 0, 3, 0, 4, 7, 1, 2, 2, 2, 1, 6, 2, 1, 3, 7, 2, 5,
-        5, 7, 7, 0, 1, 3, 1, 4, 0, 6, 5, 2 };
+    uint8_t symbols[] = {3, 1, 4, 0, 6, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                         1, 4, 0, 2, 6, 0, 0, 0, 3, 3, 0, 6, 0, 3, 6, 3,
+                         3, 5, 6, 4, 3, 1, 4, 0, 6, 5, 2, 1, 7, 4, 6, 4,
+                         2, 0, 0, 3, 0, 4, 7, 1, 2, 2, 2, 1, 6, 2, 1, 3,
+                         7, 2, 5, 5, 7, 7, 0, 1, 3, 1, 4, 0, 6, 5, 2};
     // uint8_t symbols[80] = { 0 };
-    
+
     /* TESTING */
     uint8_t other_symbols[512] = {0};
     generate_ft8_tones("CQ VE3NNE FN25", other_symbols);
-    for(int ii = 0; ii < 79; ii++) {
+    for (int ii = 0; ii < 79; ii++) {
         putchar('0' + symbols[ii]);
         putchar('0' + other_symbols[ii]);
         putchar(':');
@@ -179,10 +185,10 @@ int main(int argc, char **argv) {
 
     // 1225Hz above the dial frequency for 20m FT8
     const int base = 14074000 + 1225;
-    //const int base = 7074000 + 1225;
+    // const int base = 7074000 + 1225;
     signal(SIGINT, cleanup);
 
-    if(argc > 1) {
+    if (argc > 1) {
         si5351aSetFrequency(atoi(argv[1]));
         printf("Transmitting a test tone of %s.\n", argv[1]);
         printf("Press <return> to exit.\n");
@@ -193,9 +199,9 @@ int main(int argc, char **argv) {
 
     struct timeval tv;
     struct timezone tz;
-    for(;;) {
+    for (;;) {
         gettimeofday(&tv, &tz);
-        if((tv.tv_sec % 30) == 0) {
+        if ((tv.tv_sec % 30) == 0) {
             // Transmit in even numbered periods.
             write(1, "X", 1);
             break;
@@ -206,7 +212,7 @@ int main(int argc, char **argv) {
     usleep(500000);
 
     system("raspi-gpio set 13 dh");
-    for(int i = 0; i < 79; i++) {
+    for (int i = 0; i < 79; i++) {
         long f = base + ((symbols[i] + 1) * 6.25);
         si5351aSetFrequency(f);
         usleep(150000);
--- wspr.c
+++ wspr.c
@@ -1,14 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <pigpio.h>
 #include <inttypes.h>
-#include <unistd.h>
-#include <sys/time.h>
+#include <pigpio.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
 #include "si5351a.h"
 
 #define I2C_WRITE 0x60
-#define I2C_READ  0x61
+#define I2C_READ 0x61
 #define I2C_BUS 1
 
 //
@@ -17,11 +18,11 @@
 // num is 0..1,048,575 (0xFFFFF)
 // denom is 0..1,048,575 (0xFFFFF)
 //
-void setupPLL(int i2c, uint8_t pll, uint8_t mult, uint32_t num, uint32_t denom)
-{
-    uint32_t P1;					// PLL config register P1
-    uint32_t P2;					// PLL config register P2
-    uint32_t P3;					// PLL config register P3
+void setupPLL(int i2c, uint8_t pll, uint8_t mult, uint32_t num,
+              uint32_t denom) {
+    uint32_t P1;  // PLL config register P1
+    uint32_t P2;  // PLL config register P2
+    uint32_t P3;  // PLL config register P3
 
     P1 = (uint32_t)(128 * ((float)num / (float)denom));
     P1 = (uint32_t)(128 * (uint32_t)(mult) + P1 - 512);
@@ -34,7 +35,8 @@ void setupPLL(int i2c, uint8_t pll, uint8_t mult, uint32_t num, uint32_t denom)
     i2cWriteByteData(i2c, pll + 2, (P1 & 0x00030000) >> 16);
     i2cWriteByteData(i2c, pll + 3, (P1 & 0x0000FF00) >> 8);
     i2cWriteByteData(i2c, pll + 4, (P1 & 0x000000FF));
-    i2cWriteByteData(i2c, pll + 5, ((P3 & 0x000F0000) >> 12) | ((P2 & 0x000F0000) >> 16));
+    i2cWriteByteData(i2c, pll + 5,
+                     ((P3 & 0x000F0000) >> 12) | ((P2 & 0x000F0000) >> 16));
     i2cWriteByteData(i2c, pll + 6, (P2 & 0x0000FF00) >> 8);
     i2cWriteByteData(i2c, pll + 7, (P2 & 0x000000FF));
 }
@@ -44,24 +46,24 @@ void setupPLL(int i2c, uint8_t pll, uint8_t mult, uint32_t num, uint32_t denom)
 // R divider is the bit value which is OR'ed onto the appropriate register, it
 // is a #define in si5351a.h
 //
-void setupMultisynth(int i2c, uint8_t synth, uint32_t divider, uint8_t rDiv)
-{
-    uint32_t P1;					// Synth config register P1
-    uint32_t P2;					// Synth config register P2
-    uint32_t P3;					// Synth config register P3
+void setupMultisynth(int i2c, uint8_t synth, uint32_t divider, uint8_t rDiv) {
+    uint32_t P1;  // Synth config register P1
+    uint32_t P2;  // Synth config register P2
+    uint32_t P3;  // Synth config register P3
 
     P1 = 128 * divider - 512;
-    P2 = 0;							// P2 = 0, P3 = 1 forces an integer value for the divider
+    P2 = 0;  // P2 = 0, P3 = 1 forces an integer value for the divider
     P3 = 1;
 
-    i2cWriteByteData(i2c, synth + 0,   (P3 & 0x0000FF00) >> 8);
-    i2cWriteByteData(i2c, synth + 1,   (P3 & 0x000000FF));
-    i2cWriteByteData(i2c, synth + 2,   ((P1 & 0x00030000) >> 16) | rDiv);
-    i2cWriteByteData(i2c, synth + 3,   (P1 & 0x0000FF00) >> 8);
-    i2cWriteByteData(i2c, synth + 4,   (P1 & 0x000000FF));
-    i2cWriteByteData(i2c, synth + 5,   ((P3 & 0x000F0000) >> 12) | ((P2 & 0x000F0000) >> 16));
-    i2cWriteByteData(i2c, synth + 6,   (P2 & 0x0000FF00) >> 8);
-    i2cWriteByteData(i2c, synth + 7,   (P2 & 0x000000FF));
+    i2cWriteByteData(i2c, synth + 0, (P3 & 0x0000FF00) >> 8);
+    i2cWriteByteData(i2c, synth + 1, (P3 & 0x000000FF));
+    i2cWriteByteData(i2c, synth + 2, ((P1 & 0x00030000) >> 16) | rDiv);
+    i2cWriteByteData(i2c, synth + 3, (P1 & 0x0000FF00) >> 8);
+    i2cWriteByteData(i2c, synth + 4, (P1 & 0x000000FF));
+    i2cWriteByteData(i2c, synth + 5,
+                     ((P3 & 0x000F0000) >> 12) | ((P2 & 0x000F0000) >> 16));
+    i2cWriteByteData(i2c, synth + 6, (P2 & 0x0000FF00) >> 8);
+    i2cWriteByteData(i2c, synth + 7, (P2 & 0x000000FF));
 }
 
 //
@@ -69,8 +71,7 @@ void setupMultisynth(int i2c, uint8_t synth, uint32_t divider, uint8_t rDiv)
 // Example: si5351aOutputOff(SI_CLK0_CONTROL);
 // will switch off output CLK0
 //
-void si5351aOutputOff(uint8_t clk)
-{
+void si5351aOutputOff(uint8_t clk) {
     int i2c = i2cOpen(I2C_BUS, I2C_WRITE, 0);
 
     i2cWriteByteData(i2c, clk, 0x80);
@@ -78,7 +79,7 @@ void si5351aOutputOff(uint8_t clk)
     i2cClose(i2c);
 }
 
-// 
+//
 // Set CLK0 output ON and to the specified frequency
 // Frequency is in the range 1MHz to 150MHz
 // Example: si5351aSetFrequency(10000000);
@@ -86,8 +87,7 @@ void si5351aOutputOff(uint8_t clk)
 //
 // This example sets up PLL A and MultiSynth 0 and produces the output on CLK0
 //
-void si5351aSetFrequency(float frequency)
-{
+void si5351aSetFrequency(float frequency) {
     uint64_t pllFreq;
     uint64_t xtalFreq = XTAL_FREQ;
     uint64_t l;
@@ -99,31 +99,36 @@ void si5351aSetFrequency(float frequency)
 
     int i2c = i2cOpen(I2C_BUS, I2C_WRITE, 0);
 
-    divider = 800000000 / frequency;// Calculate the division ratio. 900,000,000 is the maximum internal
+    divider = 800000000 / frequency;  // Calculate the division ratio.
+                                      // 900,000,000 is the maximum internal
     // PLL frequency: 900MHz
-    if (divider % 2) divider--;		// Ensure an even integer division ratio
+    if (divider % 2) divider--;  // Ensure an even integer division ratio
 
-    pllFreq = divider * frequency;	// Calculate the pllFrequency: the divider * desired output frequency
+    pllFreq = divider * frequency;  // Calculate the pllFrequency: the divider *
+                                    // desired output frequency
 
-    mult = pllFreq / xtalFreq;		// Determine the multiplier to get to the required pllFrequency
-    l = pllFreq % xtalFreq;			// It has three parts:
-    f = l;							// mult is an integer that must be in the range 15..90
-    f *= 1048575;					// num and denom are the fractional parts, the numerator and denominator
-    f /= xtalFreq;					// each is 20 bits (range 0..1048575)
-    num = f;						// the actual multiplier is  mult + num / denom
-    denom = 1048575;				// For simplicity we set the denominator to the maximum 1048575
+    mult = pllFreq / xtalFreq;  // Determine the multiplier to get to the
+                                // required pllFrequency
+    l = pllFreq % xtalFreq;     // It has three parts:
+    f = l;          // mult is an integer that must be in the range 15..90
+    f *= 1048575;   // num and denom are the fractional parts, the numerator and
+                    // denominator
+    f /= xtalFreq;  // each is 20 bits (range 0..1048575)
+    num = f;        // the actual multiplier is  mult + num / denom
+    denom = 1048575;  // For simplicity we set the denominator to the maximum
+                      // 1048575
 
     // Set up PLL A with the calculated multiplication ratio
     setupPLL(i2c, SI_SYNTH_PLL_A, mult, num, denom);
-    // Set up MultiSynth divider 0, with the calculated divider. 
-    // The final R division stage can divide by a power of two, from 1..128. 
-    // reprented by constants SI_R_DIV1 to SI_R_DIV128 (see si5351a.h header file)
-    // If you want to output frequencies below 1MHz, you have to use the 
+    // Set up MultiSynth divider 0, with the calculated divider.
+    // The final R division stage can divide by a power of two, from 1..128.
+    // reprented by constants SI_R_DIV1 to SI_R_DIV128 (see si5351a.h header
+    // file) If you want to output frequencies below 1MHz, you have to use the
     // final R division stage
     setupMultisynth(i2c, SI_SYNTH_MS_0, divider, SI_R_DIV_8);
-    // Reset the PLL. This causes a glitch in the output. For small changes to 
+    // Reset the PLL. This causes a glitch in the output. For small changes to
     // the parameters, you don't need to reset the PLL, and there is no glitch
-    i2cWriteByteData(i2c, SI_PLL_RESET, 0xA0);	
+    i2cWriteByteData(i2c, SI_PLL_RESET, 0xA0);
     // Finally switch on the CLK0 output (0x4F)
     // and set the MultiSynth0 input to be PLL A
     i2cWriteByteData(i2c, SI_CLK0_CONTROL, 0x4F | SI_CLK_SRC_PLL_A);
@@ -141,14 +146,21 @@ void cleanup(int err) {
 int main(int argc, char **argv) {
     gpioInitialise();
     // VE3XEU FN25 1.995W
-    char symbols[] = { 3, 1, 2, 0, 0, 0, 2, 0, 1, 2, 2, 0, 3, 3, 3, 0, 2, 2, 3, 2, 2, 3, 2, 3, 3, 3, 3, 0, 2, 2, 2, 2, 0, 2, 3, 0, 2, 1, 2, 3, 0, 0, 0, 2, 0, 0, 1, 0, 3, 1, 2, 2, 3, 1, 2, 3, 2, 2, 2, 1, 3, 0, 1, 2, 2, 0, 2, 3, 1, 2, 3, 2, 1, 2, 3, 0, 3, 2, 2, 3, 2, 2, 1, 2, 1, 1, 2, 0, 0, 3, 3, 0, 1, 2, 1, 2, 0, 0, 1, 0, 0, 0, 2, 2, 1, 2, 2, 1, 2, 2, 1, 3, 1, 0, 3, 1, 2, 0, 1, 1, 0, 1, 0, 2, 2, 1, 3, 3, 2, 0, 2, 0, 0, 3, 2, 3, 2, 0, 1, 3, 2, 0, 2, 0, 0, 2, 0, 1, 3, 2, 1, 2, 1, 3, 2, 0, 0, 3, 3, 2, 0, 2 };
+    char symbols[] = {
+        3, 1, 2, 0, 0, 0, 2, 0, 1, 2, 2, 0, 3, 3, 3, 0, 2, 2, 3, 2, 2, 3, 2, 3,
+        3, 3, 3, 0, 2, 2, 2, 2, 0, 2, 3, 0, 2, 1, 2, 3, 0, 0, 0, 2, 0, 0, 1, 0,
+        3, 1, 2, 2, 3, 1, 2, 3, 2, 2, 2, 1, 3, 0, 1, 2, 2, 0, 2, 3, 1, 2, 3, 2,
+        1, 2, 3, 0, 3, 2, 2, 3, 2, 2, 1, 2, 1, 1, 2, 0, 0, 3, 3, 0, 1, 2, 1, 2,
+        0, 0, 1, 0, 0, 0, 2, 2, 1, 2, 2, 1, 2, 2, 1, 3, 1, 0, 3, 1, 2, 0, 1, 1,
+        0, 1, 0, 2, 2, 1, 3, 3, 2, 0, 2, 0, 0, 3, 2, 3, 2, 0, 1, 3, 2, 0, 2, 0,
+        0, 2, 0, 1, 3, 2, 1, 2, 1, 3, 2, 0, 0, 3, 3, 2, 0, 2};
 
     // 1800Hz above the dial frequency for 20m FT8
     const int base = 8 * (14095600 + 1700);
-    //const int base = 8 * (7038600 + 1700);
+    // const int base = 8 * (7038600 + 1700);
     signal(SIGINT, cleanup);
 
-    if(argc > 1) {
+    if (argc > 1) {
         si5351aSetFrequency(atoi(argv[1]) * 8);
         printf("Transmitting a test tone of %s.\n", argv[1]);
         printf("Press <return> to exit.\n");
@@ -159,23 +171,23 @@ int main(int argc, char **argv) {
 
     struct timeval tv;
     struct timezone tz;
-    for(int x = 0; x < 3; x++) {
-    for(;;) {
-        gettimeofday(&tv, &tz);
-        if((tv.tv_sec % 120) == 1) {
-            // Transmit in even numbered minutes.
-            write(1, "X", 1);
-            break;
+    for (int x = 0; x < 3; x++) {
+        for (;;) {
+            gettimeofday(&tv, &tz);
+            if ((tv.tv_sec % 120) == 1) {
+                // Transmit in even numbered minutes.
+                write(1, "X", 1);
+                break;
+            }
+            write(1, ".", 1);
+            usleep(100000);
         }
-        write(1, ".", 1);
-        usleep(100000);
-    }
 
-    for(int i = 0; i < 162; i++) {
-        float f = (base) + ((symbols[i] + 0) * 12); // was 14
-        si5351aSetFrequency(f);
-        usleep(680000); // 110.6 / 162
-    }
+        for (int i = 0; i < 162; i++) {
+            float f = (base) + ((symbols[i] + 0) * 12);  // was 14
+            si5351aSetFrequency(f);
+            usleep(680000);  // 110.6 / 162
+        }
         si5351aSetFrequency(base - 1000);
     }
     cleanup(0);
